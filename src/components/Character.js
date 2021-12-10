5739ecf6eb0891f1461632ce8208b56007a309cc
Douglas Wilson <dougwilson0000@gmail.com>
2021-12-10 17:52:24 -0500

    completed app.js and character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = (props) => {
+    
+    const chars = (props.charList);
+    
+    
+    return (
+        <article> 
+{
+    chars.map(character => (
+        <h1 key={character.name}>{character.name}</h1>
+    ))
+
+}
+
+        </article>);
+}
+
+
+
+export default Character;
+
+
+
+
+
